49e39d034e6ca1367ce7eccc8f7cb1709d718cd6
AnotherSlime <123135276+AnotherSlime@users.noreply.github.com>
2023-02-03 20:06:58 -0500

    memorization/caching

--- netlify/functions/list-skins.ts
+++ netlify/functions/list-skins.ts
@@ -1,7 +1,17 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import axios from "axios";
 
+let cachedPage = "";
+let cachedResp = "";
+
 const notString = (val: any) => !(typeof val === "string" && val);
+const resp = (body: string) => ({
+    statusCode: 200,
+    body: body,
+    // headers: {
+    //     "Cache-Control": "public, s-maxage=10"
+    // }
+});
 
 const handler: Handler = async (
     event: HandlerEvent,
@@ -11,19 +21,28 @@ const handler: Handler = async (
         const mainPage = await axios.get("https://www.gimkit.com/");
         const mainHtml = mainPage?.data;
         if (notString(mainHtml)) throw new Error("main html is not string");
+
+        if (cachedPage && mainHtml === cachedPage) {
+            return resp(cachedResp);
+        }
+        console.log("new!");
+
         const jsPath = mainHtml.match(
             /<script type="module" src="(\/index\..*?\.js)"><\/script>/
         )?.[1];
         if (notString(jsPath))
             throw new Error("could not find js path in main html");
+
         const mainJsPage = await axios.get("https://www.gimkit.com/" + jsPath);
         const mainJs = mainJsPage?.data;
         if (notString(mainJs)) throw new Error("main js is not string");
+
         const skinsObj = mainJs.match(
             /{(?:default\w+?:(\w+?)\("\w+?"\))(?:,\w+?:\1\("\w+?"\))+?}/i
         )?.[0];
         if (notString(skinsObj))
             throw new Error("could not find skins object in main js");
+
         const skinsArr = skinsObj.match(/(?<=\w+?:\w+?\(")\w+?(?="\))/g);
         if (
             !(
@@ -34,10 +53,14 @@ const handler: Handler = async (
         )
             throw new Error("could not find skins array in main js");
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(skinsArr.reverse()),
-        };
+        const body = JSON.stringify(skinsArr.reverse());
+
+        if (!(cachedPage && mainHtml === cachedPage)) {
+            cachedPage = mainHtml;
+            cachedResp = body;
+        }
+
+        return resp(body);
     } catch (e) {
         console.log(e);
         return {
